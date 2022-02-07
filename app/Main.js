507dcd5f62911c4e49d86640268af2d2cdef1b35
Nicole George <hello@nicolegeorge.me>
2022-02-07 14:45:03 +1030

    adding router package and updating page routes and links

--- app/Main.js
+++ app/Main.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
+import About from './components/About';
+import Terms from './components/Terms';
 import HomeGuest from './components/HomeGuest';
 import Footer from './components/Footer';
 
 const Main = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path='/' element={<HomeGuest />} />
+        <Route path='/about-us' element={<About />} />
+        <Route path='/terms' element={<Terms />} />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
