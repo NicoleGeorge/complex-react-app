3a9685071966c642e4b6cc924362fb68fa6d80a2
Nicole George <hello@nicolegeorge.com.au>
2022-04-01 14:41:27 +1030

    update

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HeaderLoggedOut from './HeaderLoggedOut';
+import HeaderLoggedIn from './HeaderLoggedIn';
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState();
+
+
   return (
     <header className='header-bar bg-primary mb-3'>
       <div className='container d-flex flex-column flex-md-row align-items-center p-3'>
@@ -11,7 +15,7 @@ const Header = () => {
             ComplexApp
           </Link>
         </h4>
-        <HeaderLoggedOut />
+        {loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
       </div>
     </header>
   );
